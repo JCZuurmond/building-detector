d4215e5feb2f6c8241708a95d298f33764baf6f4
Cor Zuurmond <jczuurmond@protonmail.com>
2019-12-24 12:18:21 +0100

    Move gps_to_tile to helpers

--- building_detector/wmts.py
+++ building_detector/wmts.py
@@ -1,39 +1,9 @@
-import math
 from io import BytesIO
 
 import requests
 from PIL import Image
 
-
-def gps_to_tile(lat, lon, zoom):
-    """
-    Convert gps coordinate to tile number.
-
-    Parameters
-    ----------
-    lat : float
-        The latitude of the coordinate.
-    lon : float
-        The longitude of the coordinate.
-    zoom : int
-        Zoom level.
-
-    Returns
-    -------
-    Tuple(int, int) : The tile (column, row) index.
-
-    Source
-    ------
-    https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Tile_numbers_to_lon..2Flat._2
-    """
-    n = 2.0 ** zoom
-    lat_rad = math.radians(lat)
-    sec = (1 / math.cos(lat_rad))
-
-    tile_col = (lon + 180.0) / 360.0 * n
-    tile_row = (1.0 - math.log(math.tan(lat_rad) + sec) / math.pi) / 2.0 * n
-
-    return int(tile_col), int(tile_row)
+from .helpers import gps_to_tile
 
 
 def get_wmts_tile_from_coordinate(coordinate, wmts_base, layer, zoom=0):
